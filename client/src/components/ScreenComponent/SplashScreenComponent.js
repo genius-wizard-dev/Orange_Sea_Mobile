d5bfdd6d23f767079467282ef99adca40d1c3ca5
phong <nguyenphonghk2003@gmail.com>
2025-04-10 12:05:43 +0700

    init project exits

---
+++ client/src/components/ScreenComponent/SplashScreenComponent.js
@@ -0,0 +1,45 @@
+import { View, Text, StyleSheet, StatusBar, Image } from 'react-native'
+import React, { useEffect } from 'react'
+
+const logo_img = require("../../../assets/logo_icon_text.png")
+
+
+const SplashScreenComponent = ({ navigation }) => {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigation.replace('ContainerAuthPage');
+        }, 900);
+        return () => clearTimeout(timer);
+    }, [navigation]);
+
+    return (
+        <View style={styles.container}>
+            <StatusBar hidden={true} style="dark" />
+            {/* <Text style={styles.text}>INTRO</Text> */}
+                <Image source={logo_img} style={styles.img_logo}/>
+        </View>
+    )
+}
+
+export default SplashScreenComponent
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: '#ff6666',
+        backgroundColor: '#fff',
+        padding: 30
+    },
+    img_logo: {
+        width: "75%",
+        objectFit: "contain",
+        borderRadius:20
+    },
+    text: {
+        color: "#fff",
+        fontSize: 35,
+        fontWeight: "700",
+    },
+});
---
+++ client/src/styleMixins/@minxin.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export const color_main = "#FE781F";
+
+export const stylesMixin  = StyleSheet.create({
+
+    buttonComponent:{
+        width:"auto",
+        padding:20,
+        backgroundColor:color_main,
+        borderRadius:15,
+        justifyContent:"center",
+        alignItems:"center",
+        color:"#fff"
+    },
+    
+})
+
+// #FE781F
+
+
+// #eeeeee (238,238,238)
+// #ffcccc	(255,204,204)
+// #fcb0b1	(252,176,177)
+// #ff9999	(255,153,153)
+// #ff6666	(255,102,102)
