4f65c942f7e3af695b48fdfcc4a03dd2e5f53874
phong <nguyenphonghk2003@gmail.com>
2025-04-11 20:56:16 +0700

    #2 write redux client

--- client/src/redux/thunks/authThunk.js
+++ client/src/redux/thunks/authThunk.js
@@ -7,10 +7,18 @@ export const registerUser = createAsyncThunk(
     async (CredentialsContainer, {rejectWithValue})=>{
         try {
             
-            // const res = await
+            const res = await post(ENDPOINT.REGISTER_USER, registerUser);
+            if(!res.data.token){
+                throw new Error("token khong hop le");
+            }
+            return res.data;
 
         } catch (error) {
-            
+            if (error.response) {
+                return rejectWithValue(error.response.data);
+            } else {
+                return rejectWithValue({ message: "Lỗi không xác định thunks - server bị lỗi/chưa khởi động" });
+            }
         }
     }
 )
